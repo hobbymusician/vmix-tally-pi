2627ddd660ed7eacd5c212c21887fd378add9b09
hobbymusician <michael.webler@web.de>
2021-05-01 18:29:37 +0200

    Errorhandling added

--- vmix-tally-pi/tally.py
+++ vmix-tally-pi/tally.py
@@ -1,14 +1,109 @@
 #!/usr/bin/env python2
-import sys 
 import time
-import pycurl
-from io import BytesIO
+import sys
+from enum import Enum
+
 from colorsys import hsv_to_rgb
+from PIL import Image, ImageDraw, ImageFont
 from unicornhatmini import UnicornHATMini
 
-brightness=1.0
-remotePort="8080"
+import pycurl
+from io import BytesIO
 
+class tallyState(Enum):
+	SEARCHING_CAM = 1
+	CAM_FOUND = 2
+	
+def displayError(text):
+	unicornhatmini.set_brightness(0.1)
+	hue = 1.0
+	r, g, b = [255, 255, 255]
+	unicornhatmini.set_all(0, 0, 0)
+	unicornhatmini.show()
+	
+	text_width, text_height = font.getsize(text)
+	image = Image.new('P', (text_width + display_width + display_width, display_height), 0)
+	draw = ImageDraw.Draw(image)
+	draw.text((display_width, -1), text, font=font, fill=255)
+	offset_x = 0
+
+	while True:
+		for y in range(display_height):
+			for x in range(display_width):
+				hue = 1
+				r, g, b = [255, 255, 255]
+				if image.getpixel((x + offset_x, y)) == 255:
+					unicornhatmini.set_pixel(x, y, r, g, b)
+				else:
+					unicornhatmini.set_pixel(x, y, 0, 0, 0)
+
+		offset_x += 1
+		if offset_x + display_width > image.size[0]:
+			return
+
+		unicornhatmini.show()
+		time.sleep(0.05)
+
+def searchCam():
+	# Determine key for camera with given name
+	b_obj=BytesIO()
+	try: 
+		crl=pycurl.Curl()
+		urlstr="http://"+ipAddressServer+":"+remotePort+"/tally"
+		crl.setopt(crl.URL, urlstr)
+		crl.setopt(crl.WRITEDATA, b_obj)
+		crl.perform()
+		crl.close()
+	except pycurl.error as exc:
+		errstr="Unable to reach %s (%s)" % (urlstr , exc)
+		print (errstr)
+		displayError(errstr)
+		return ""			
+	get_body = b_obj.getvalue()
+	setupstr=get_body.decode('utf8')
+	b_obj.close()
+	prefix="\" class=\"tallyLink\">"+cameraName+"</a>"
+	endpos=setupstr.find(prefix)
+	startpos=endpos-36
+	if startpos>0:
+		keystr=setupstr[startpos:endpos]
+		urlstr="http://"+ipAddressServer+":"+remotePort+"/tallyupdate/?key="+keystr
+		print ("Trying to fetch tally status from '"+urlstr+"'")
+		return urlstr
+	else:
+		print ("Did not find camera '"+cameraName+"'!")
+		displayError("Did not find camera '"+cameraName+"'!")
+		return ""
+	
+	
+def readColor(urlstr):
+	b_obj=BytesIO()
+	try: 
+		crl=pycurl.Curl()
+		crl.setopt(crl.URL, urlstr)
+		crl.setopt(crl.WRITEDATA, b_obj)
+		crl.perform()
+		crl.close()
+	except pycurl.error as exc:
+		errstr="Unable to reach %s (%s)" % (urlstr , exc)
+		print (errstr)
+		displayError(errstr)		
+		return False	
+	get_body = b_obj.getvalue()
+	changestr=get_body.decode('utf8')
+	a, colour, b = changestr.split("\"")
+	colour=colour[1:]
+	#print('%s' % colour)
+	redstr=colour[:2]
+	greenstr=colour[2:4]
+	bluestr=colour[4:]
+	r, g, b = [int(redstr, 16), int(greenstr, 16), int(bluestr, 16)]
+	unicornhatmini.set_all(r, g, b)
+	unicornhatmini.show()
+	b_obj.close()
+	return True
+
+	
 count=0
 for argument in sys.argv:
 	if (count==1):
@@ -23,68 +118,38 @@ if (count<3 or count >=5):
 	print("Usage: tally.p ""<Kamera Name>"" <ip_address> [<port>] ")
 	exit()
 
+remotePort="8088"
+
+
 print("""vMix Tally Light
+================
 Please make sure that the Web Controller in vMix 
 on the host with IP address '{0:s}' is activated under
 'Settings | Web Controller' for port '{1:d}'
-and that your camera is named '{2:s}'!""".format(ipAddressServer, int(remotePort), cameraName))
+and that your camera is named '{2:s}'!
+""".format(ipAddressServer, int(remotePort), cameraName))
 
+# Initialize Unicorn Hat Mini
+brightness=1.0
 unicornhatmini = UnicornHATMini()
-unicornhatmini.set_brightness(0)
+rotation = 0
+unicornhatmini.set_rotation(rotation)
+display_width, display_height = unicornhatmini.get_shape()
+font = ImageFont.truetype("MiniFont.ttf", 8)
 
-# Determine key for camera with given name
-b_obj=BytesIO()
-crl=pycurl.Curl()
-crl.setopt(crl.URL, 'http://192.168.77.40:8088/tally')
-crl.setopt(crl.WRITEDATA, b_obj)
-crl.perform()
-crl.close()
-get_body = b_obj.getvalue()
-print('%s' % get_body.decode('utf8'))
-setupstr=get_body.decode('utf8')
-b_obj.close()
-prefix="\" class=\"tallyLink\">"+cameraName+"</a>"
-endpos=setupstr.find(prefix)
-print prefix
-print endpos
-startpos=endpos-36
-if startpos>0:
-	keystr=setupstr[startpos:endpos]
-	urlstr="http://"+ipAddressServer+":"+remotePort+"/tallyupdate/?key="+keystr
-	print urlstr
-else:
-	print "Did not find camera '"+cameraName+"'!"
-	exit()
-	
-
-unicornhatmini.set_brightness(1)
-hue = 1.0
-r, g, b = [255, 255, 255]
-unicornhatmini.set_all(r, g, b)
-unicornhatmini.show()
+# Initial state within the infinite loop
+state=tallyState.SEARCHING_CAM
 
 while True:
-	b_obj=BytesIO()
-	crl=pycurl.Curl()
-	#crl.setopt(crl.URL, 'http://192.168.77.40:8088/tallyupdate/?key=7befea86-01e1-44b0-a332-5c4ad8709f42')
-	crl.setopt(crl.URL, urlstr)
-	crl.setopt(crl.WRITEDATA, b_obj)
-	crl.perform()
-	crl.close()
-	get_body = b_obj.getvalue()
-	#print('%s' % get_body.decode('utf8'))
-	changestr=get_body.decode('utf8')
-	#changestr="tallyChange(\"#ff8c00\");"
-	#changestr="tallyChange(\"#006400\");"
-	a, colour, b = changestr.split("\"")
-	#colour.removeprefix('#');
-	colour=colour[1:]
-	#print('%s' % colour)
-	redstr=colour[:2]
-	greenstr=colour[2:4]
-	bluestr=colour[4:]
-	r, g, b = [int(redstr, 16), int(greenstr, 16), int(bluestr, 16)]
-	unicornhatmini.set_all(r, g, b)
-	unicornhatmini.show()
-	b_obj.close()
+	if state == tallyState.SEARCHING_CAM:
+		unicornhatmini.set_brightness(0)
+		camUrlStr=searchCam()
+		if camUrlStr != "":
+			state=tallyState.CAM_FOUND
+			unicornhatmini.set_brightness(0.5)
+			hue = 1.0
+	elif state == tallyState.CAM_FOUND:
+		if not readColor(camUrlStr):
+			state=tallyState.SEARCHING_CAM
 	time.sleep(0.2)
+		
